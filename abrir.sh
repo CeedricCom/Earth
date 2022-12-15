dff404b4f5d292acc4e1381b43070239ec14a040
DeltaOrion <specdartpokeman@gmail.com>
2022-12-15 11:32:14 +0000

    LAG BUSTING!

--- abrir.sh
+++ abrir.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms17G -Xmx17G -jar paper-custompots-1-19-3.jar
+java -Xms13G -Xmx13G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1MaxNewSizePercent=60 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar paper-custompots-1-19-3.jar
