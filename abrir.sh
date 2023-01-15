5fbb74e50d76af7145df9b4461c364c45ffdf8b0
ceeedric <fabiocedric@gmail.com>
2023-01-15 09:19:46 +0000

    Fix Siegewar sessions, use better server software

--- abrir.sh
+++ abrir.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-	java -Xms13G -Xmx13G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1MaxNewSizePercent=60 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar paper-custompots-371.jar
+	java -Xms13G -Xmx13G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1MaxNewSizePercent=60 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M --add-modules=jdk.incubator.vector -jar purpur-custompots-371.jar
 	echo "Restarting in 5 seconds. Press CTRL + C to cancel."
 	sleep 5s
 done
